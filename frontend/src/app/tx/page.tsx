8ea16381ac19fa0dd84d9aee6f546e1fd1410614
caso <matteocasonato@protonmail.com>
2024-07-12 22:45:45 +0200

    init frontend

---
+++ frontend/src/app/tx/page.tsx
@@ -0,0 +1,8 @@
+export default function Tx() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Tx page :)
+      <w3m-button />
+    </main>
+  );
+}
