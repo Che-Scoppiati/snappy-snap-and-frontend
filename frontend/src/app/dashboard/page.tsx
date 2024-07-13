7a15aed296d97f3b82e74a4b8e8de134635cbb48
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-07-13 20:43:56 +0200

    fetch transaction from blockscout

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -3,21 +3,33 @@
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { GetTxResult } from "../tx/[transactionId]/page";
+import axios, { AxiosResponse } from "axios";
 
 const DashboardPage = () => {
   const [transactions, setTransactions] = useState<GetTxResult[]>([]);
   const account = useAccount();
+  // go here to see other chains https://www.blockscout.com/chains-and-projects
   const baseBlockscout = "https://eth-sepolia.blockscout.com";
   console.log("chain", account.chain);
 
   useEffect(() => {
     async function fetchTransactions() {
+      const bloc = await getBlockscoutTransaction(
+        "0x37c2e21bc8fe1977e3b9e8eafebe71774e28d1cf64b01988ece77cb8094c1a70",
+      );
+      console.log("bloc", bloc);
       const response = await fetch(`/api/tx/`);
       const data = await response.json();
       setTransactions(data.tx.data);
     }
     fetchTransactions();
   }, []);
+
+  async function getBlockscoutTransaction(txHash: string) {
+    // reference https://eth-sepolia.blockscout.com/api-docs#operations-default-get_tx
+    const data = axios.get(`${baseBlockscout}/api/v2/transactions/${txHash}`);
+    return data;
+  }
 
   return (
     <div className="flex flex-col text-white">
